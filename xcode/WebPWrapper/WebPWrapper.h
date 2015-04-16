e2e8e29b355b9061b4334c16d488d2a5bf03ccbc
Cosmin Gordea <cosmin.gordea@arobs.com>
2015-04-16 12:15:08 +0300

    xcode wrapper project

---
+++ xcode/WebPWrapper/WebPWrapper.h
@@ -0,0 +1,13 @@
+//
+//  WebPWrapper.h
+//  WebPWrapper
+//
+//  Created by Cosmin G on 16/04/15.
+//  Copyright (c) 2015 Cosmin G. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface WebPWrapper : NSObject
+
+@end
---
+++ xcode/WebPWrapper/WebPWrapper.m
@@ -0,0 +1,13 @@
+//
+//  WebPWrapper.m
+//  WebPWrapper
+//
+//  Created by Cosmin G on 16/04/15.
+//  Copyright (c) 2015 Cosmin G. All rights reserved.
+//
+
+#import "WebPWrapper.h"
+
+@implementation WebPWrapper
+
+@end
